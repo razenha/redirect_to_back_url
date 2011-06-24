0e5faf499a3a5a0cccb110a93bcc58240b675a01
razenha <rubem.azenha@gmail.com>
2011-06-24 06:50:56 -0300

    Redirect to back - controller and app helper

--- lib/redirect_to_back_url/application_helper.rb
+++ lib/redirect_to_back_url/application_helper.rb
@@ -1,5 +1,5 @@
 module RedirectToBackUrl
-  module Controller
+  module ApplicationHelper
     def save_back_url(back_url_key = "back_url")
       url = params[back_url_key] || request.referer
       return "<input type='hidden' name='#{back_url_key}' value='#{url}' />" 
