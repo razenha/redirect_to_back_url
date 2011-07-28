4d5565ea7b6e351317957eeae9cde908819828d5
razenha <rubem.azenha@gmail.com>
2011-07-28 17:20:55 -0300

    ApplicationHelper.save_back_url returns nil if no param nor referer is provided

--- lib/redirect_to_back_url/application_helper.rb
+++ lib/redirect_to_back_url/application_helper.rb
@@ -2,7 +2,7 @@ module RedirectToBackUrl
   module ApplicationHelper
     def save_back_url(back_url_key = "back_url")
       url = params[back_url_key] || request.referer
-      return "<input type='hidden' name='#{back_url_key}' value='#{url}' />" 
+      return "<input type='hidden' name='#{back_url_key}' value='#{url}' />" unless url.blank?
     end
   end
 end
