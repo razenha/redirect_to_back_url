7789f98c41175fd50f62a9b1b1f239f510e0af15
razenha <rubem.azenha@gmail.com>
2012-07-13 15:51:34 -0300

    Rails 3 compatibility

--- lib/redirect_to_back_url/application_helper.rb
+++ lib/redirect_to_back_url/application_helper.rb
@@ -2,7 +2,11 @@ module RedirectToBackUrl
   module ApplicationHelper
     def save_back_url(back_url_key = "back_url")
       url = params[back_url_key] || request.referer
-      return "<input type='hidden' name='#{back_url_key}' value='#{url}' />" unless url.blank?
+      return "<input type='hidden' name='#{back_url_key}' value='#{url}' />".html_safe unless url.blank?
+    end
+    
+    def url_for_back_url(back_url_key = "back_url", failsafe = url_for(:action => :index))
+      params[back_url_key] || request.referer || failsafe
     end
   end
 end
